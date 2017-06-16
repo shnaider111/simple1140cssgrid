2e1dcb5cb88f60cdfb1f3b1029347db82c087b60
shnaider <shnaider@land.ru>
2017-06-16 09:36:09 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="container">
+		<div class="row">
+			<div class="cols col-4 col-s-6 col-m-4">1</div>
+			<div class="cols col-4 col-s-6 col-m-4">2</div>
+			<div class="cols col-4 col-s-6 col-m-4">3</div>
+		</div>
+		<div class="row">
+			<div class="cols col-4 col-s-6 col-m-4">4</div>
+			<div class="cols col-4 col-s-6 col-m-4">5</div>
+			<div class="cols col-4 col-s-6 col-m-4">6</div>
+		</div>
+    </div>
+</body>
+</html>
